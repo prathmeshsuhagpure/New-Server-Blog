b4e85f27e01987a91916c4cc12b080a2f252d67d
Prathmesh Suhagpure <prathmeshsuhagpure3@gmail.com>
2024-12-19 15:08:58 +0530

    changes made

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const serverless = require("serverless-http");
 
+const Port = process.env.port || 5000
+
 const app = express();
 
 // MongoDB connection
@@ -15,11 +17,20 @@ connection.once("open", () => {
 // Middleware
 app.use(express.json());
 
+// Middleware for error handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: "Internal Server Error", message: err.message });
+});
+
+
 // Routes
 const userRoute = require("../routes/user"); // Adjust the path based on your folder structure
 app.use("/.netlify/functions/server/user", userRoute);
 
 app.get("/.netlify/functions/server", (req, res) => res.json("My First REST API"));
+
+app.listen(Port, () => console.log(`Server is running on port ${Port}`));
 
 // Export for Netlify Functions
 module.exports.handler = serverless(app);
